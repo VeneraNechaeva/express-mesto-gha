8c0786a9db960d8fa45f2686e78b04afe7c14a75
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-08-09 11:40:34 +0300

    feat: добавить пробный Joi

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,66 @@
+const { celebrate, Joi } = require('celebrate');
+
 const router = require('express').Router();
 const {
   getUsers, getUserById, updateUser, updateUserAvatar, getCurrentUser,
 } = require('../controllers/users');
 
 /// /// /// Роут для получения информации о текущем пользователе
-router.get('/users/me', getCurrentUser);
+router.get('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi)),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), getCurrentUser);
 
-router.get('/users', getUsers);
+router.get('/users',
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi)),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+getUsers);
 
-router.get('/users/:userId', getUserById);
+router.get('/users/:userId', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi)),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+getUserById);
 
 /// /// ///
-router.patch('/users/me', updateUser);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi)),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+updateUser);
 
-router.patch('/users/me/avatar', updateUserAvatar);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+
+updateUserAvatar);
 
 module.exports = router;

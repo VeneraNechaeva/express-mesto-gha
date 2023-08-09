a2e8300a4fffd686d003d375c161d9e3985584d4
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-08-09 11:40:31 +0300

    feat: добавить пробный Joi

---
+++ validators/user_validator.js
@@ -0,0 +1,11 @@
+// // Импорт иблиотеки для валидации данных запроса
+// const { celebrate, Joi } = require('celebrate');
+
+// body: Joi.object().keys({
+//   name: Joi.string().min(2).max(30),
+//   about: Joi.string().min(2).max(30),
+//   avatar: Joi.string().pattern(new RegExp(/https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=]+$/gi)),
+//   email: Joi.string().required().email(),
+//   password: Joi.string().required().min(8),
+// })
+// }
